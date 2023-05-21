b3acbc8dcba42b8e4f3c2c108529d608bb86d218
Greg Ederer <greg@makereal.com>
2023-05-21 10:07:13 -0700

    initial commit

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,25 +1,35 @@
-import { component$ } from '@builder.io/qwik';
-import type { DocumentHead } from '@builder.io/qwik-city';
+import { component$ } from "@builder.io/qwik";
+import type { DocumentHead} from "@builder.io/qwik-city";
+import { routeLoader$ } from "@builder.io/qwik-city";
+import { db } from "@vercel/postgres";
+
+export const useRoleLoader = routeLoader$(async () => {
+  const client = await db.connect();
+  const roles = await client.sql`SELECT * FROM "Role"`;
+
+  console.log(process.env);
+
+  return roles.rows;
+});
 
 export default component$(() => {
+  const roleLoader = useRoleLoader();
+
   return (
-    <>
-      <h1>Hi 👋</h1>
-      <p>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </p>
-    </>
+    <div>
+      {roleLoader.value.map(role => (
+        <div key={role.id}>{role.name}</div>
+      ))}
+    </div>
   );
 });
 
 export const head: DocumentHead = {
-  title: 'Welcome to Qwik',
+  title: "Welcome to Qwik",
   meta: [
     {
-      name: 'description',
-      content: 'Qwik site description',
+      name: "description",
+      content: "Qwik site description",
     },
   ],
 };

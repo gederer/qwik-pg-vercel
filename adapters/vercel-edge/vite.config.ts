9dc1c2bb563675b63cf1e31127b15f3f70280371
Gal Schlezinger <gal@spitfire.co.il>
2023-05-23 12:46:09 +0300

    add a plugin that modified vercel-adapter modifications

--- adapters/vercel-edge/vite.config.ts
+++ adapters/vercel-edge/vite.config.ts
@@ -11,6 +11,31 @@ export default extendConfig(baseConfig, () => {
       },
       outDir: '.vercel/output/functions/_qwik-city.func',
     },
-    plugins: [vercelEdgeAdapter()],
+    plugins: [vercelEdgeAdapter(), {
+      name: 'qwik-city:vercel-edge-fixes',
+      enforce: "post",
+      config(config) {
+        config.ssr = {
+          ...config.ssr,
+          // vercel-edge uses `target: webworker`, this makes process.env not
+          // available. We need to set it to node to make it work.
+          // we also support a subset of Node.js API in production so people can use
+          // global Buffer or `import { Buffer } from 'buffer'` so it kinda makes sense
+          // to set it to node.
+          target: "node"
+        };
+        config.resolve = {
+          ...config.resolve,
+          // this is a better condition list based on https://github.com/vercel/next.js/blob/2fc0160a2628be40794dc097814006342298583c/packages/next/src/build/webpack-config.ts#L103-L109
+          conditions: [
+            'edge-light', // the standard wintercg key for Vercel Edge Functions
+            'worker',
+            'browser',
+            'module',
+            'main',
+          ]
+        }
+      }
+    }],
   };
 });
